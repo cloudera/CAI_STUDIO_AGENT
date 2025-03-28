f36565b67b6199db0e14a80f06fda2c2cb2d35a0
Jason Everett <jev@cloudera.com>
2025-03-28 18:33:54 +0000

    remove comment

--- app/api/workbench/route.ts
+++ app/api/workbench/route.ts
@@ -7,10 +7,6 @@ const agent = new https.Agent({
   ca: fs.readFileSync('/etc/ssl/certs/ca-certificates.crt'),
 });
 
-// Extract information about the rendermode and the
-// workflow if a workflow app is initialized. This
-// is determined by env vars that are passed in at
-// application start.
 export async function GET(request: NextRequest) {
   const response = await fetch(`https://${process.env.CDSW_DOMAIN}/sense-bootstrap.json`, {
     headers: {
